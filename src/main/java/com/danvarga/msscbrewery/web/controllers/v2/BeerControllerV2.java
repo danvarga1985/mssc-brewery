8ea0d12f21d482a2a95137b00303cdc896bfcf19
danvarga1985 <danvarga1985@gmail.com>
2020-12-29 09:18:52 +0100

    Setting up Mapstruct

--- src/main/java/com/danvarga/msscbrewery/web/controllers/v2/BeerControllerV2.java
+++ src/main/java/com/danvarga/msscbrewery/web/controllers/v2/BeerControllerV2.java
@@ -2,6 +2,9 @@ package com.danvarga.msscbrewery.web.controllers.v2;
 
 import com.danvarga.msscbrewery.services.v2.BeerServiceV2;
 import com.danvarga.msscbrewery.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +13,12 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
     private final BeerServiceV2 beerServiceV2;
-
-    public BeerControllerV2(BeerServiceV2 beerServiceV2) {
-        this.beerServiceV2 = beerServiceV2;
-    }
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId) {
@@ -28,9 +29,14 @@ public class BeerControllerV2 {
     @PostMapping
     public ResponseEntity<BeerDtoV2> handlePost(@Valid @RequestBody BeerDtoV2 beerDto) {
 
-        BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
+        log.debug("in handle post...");
 
-        HttpHeaders headers = new HttpHeaders();
+        // val - infer the type from the declaration. Immutable if possible. Lombok.
+        val savedDto = beerServiceV2.saveNewBeer(beerDto);
+
+        // val - infer the type from the declaration. Lombok.
+        var headers = new HttpHeaders();
+
         // TODO: add hostname to url
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
